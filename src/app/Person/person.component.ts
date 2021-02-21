aebde8a16384e06fbd17a19714fedaba9c189a85
MadanMohanReddy <madan.patakota@hotmail.com>
2021-02-21 10:18:10 +0530

    DataBinding Concepts

--- src/app/Person/person.component.ts
+++ src/app/Person/person.component.ts
@@ -3,23 +3,55 @@ import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-person',
-  //templateUrl : './person.component.html',
-  template: `<p>Person Details : </p>
-  <p>Person Details : </p>
-  <p>Person Details : </p>
-  <p>Person Details : </p>
-  `,
+  templateUrl: './person.component.html',
+  // template: `<p>Person Details : </p>
+  // <p>Person Details : </p>
+  // <p>Person Details : </p>
+  // <p>Person Details : </p>
+  //`,
   //styleUrls : ['./person.component.css']
   styles: [
     `p{
       margin: 5px;
       font-size : large;
     }`
-  
+
   ]
 
 })
 
 export class PersonComponent {
 
+
+  IsDisabled = true;
+  inputtype = "text"
+
+  constructor() {
+    setTimeout(() => {
+      this.IsDisabled = false;
+      this.inputtype = "checkbox";
+    }, 5000)
+  }
+
+
+  customerName = "Madan";  // which may be generated from DB 
+
+  twowaycustomerName = "";
+
+  customerID = 1;   // 
+
+  getCustomerName() {
+    return this.customerName;
+  }
+
+
+  updateCustomerName() {
+    this.customerName = "John Smith";
+    console.log(this.twowaycustomerName);
+  }
+
+  evtComments(event) {
+    this.customerName = event.target.value;
+    //console.log(this.twowaycustomerName);
+  }
 }
